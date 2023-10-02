f9dca1862f9b2960bf0e2e2834d77b12dc68e704
Cheikh Gueye <bayecheikhgueye2@gmail.com>
2023-10-02 14:00:59 +0000

    update

--- components/dashboard/Statistiques.vue
+++ components/dashboard/Statistiques.vue
@@ -28,7 +28,6 @@
               </h1>
 
               <h1 class="mdi mdi-account-group-outline custom-icon-box"></h1>
-              <!-- <h1 class="mdi mdi-account-group-outline custom-icon-box"></h1> -->
 
             </div>
           </div>
